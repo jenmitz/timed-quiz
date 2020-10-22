b35b1bb7ea0138d4e3f4ee46ab2f640c6bf58a70
Jennifer Mitzman <jenmitz@gmail.com>
2020-10-21 21:46:52 -0700

    timer functionality, basic layout, cleaned up the code

--- script-2.js
+++ script-2.js
@@ -1,7 +1,96 @@
-// this entire file is for the quiz page only. get rid of the button event listener, add the timer,
-// move styling to here, and population of quiz questions
+// quiz questions page
+// main purpose: present answers and questions, redirected to when quiz starts, houses major functions like timer and nextQuestion
 
 
-// the main purpose of script.js will be to initate the quiz on button click, and link with some html
+/*
+-- IOCE --
+Inputs: start quiz, register answer chosen, save initials/score
+Outputs: question/answers, timer, game over alert, highscores
+Contraints:
+Edge cases: user clicks start but does not answer any questions
 
-// i have changed the script links, so the content needs to be moved now
+
+-- pseudocode / what's left to be done --
+1. make questions and answers appear on the page
+    - listen for click on one of the choices
+    - decide if answer was correct or not
+    - display if answer was correct or incorrect
+    - subract time if answer choice is incorrect
+2. full timer functionality
+    - seconds penalty if wrong answer is chosen
+    - stop quiz when timer hits 0 or all questions were answered
+3. display score
+
+
+-- functions -- 
+1. make question/answers appear
+function nextQuestion() {};
+
+2. timer
+
+*/
+
+// create function that makes a set of buttons (the answers), and call it each time a new question comes up
+
+
+
+// object w questions and answers
+const questions = [
+{
+    question: ("What is your quest?"),
+    choices: ["To seek the holy grail", "To seek a man called 'Tim'", "To kill the Black Knight"],
+    answer: ("To seek the holy grail")
+}, {
+    question: ("What is your favorite color?"),
+    choices: ["Blue", "Yellow", "Green"],
+    answer: ("Yellow")
+}, {
+    question: ("What is the air-speed velocity of an unladen swallow?"),
+    choices: ["An African or a European Swallow?", "30mph", "I don't know that."],
+    answer: ("An African or a European Swallow?")
+}
+];
+
+// fully functioning timer
+let timerText = $("#timerText");
+let countdown = $("#countdown");
+let secondsLeft = 30;
+
+function timer() {
+    let timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerText.text(secondsLeft);
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            timesUp();
+        };
+    }, 1000);
+};
+
+
+// message when time's up
+function timesUp() {
+    timerText.textContent = " ";
+    if (secondsLeft === 0) {
+        alert("Thy time is up!");
+    };
+    // var quizDone = document.createElement("p");
+    // countdown.appendChild(quizDone);
+};
+
+
+
+
+// attach evenlistener to each answer (choice)
+// $("#quiz").addEventListener("click", function(e) {
+//     if(!e.target.classList.contains("option"))
+//         return
+// });
+
+// function checkAnswer() {
+//     currentQuestion++;
+//     loadNextQuestion(currentQuestion)
+// };
+
+timer();
